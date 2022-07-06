257ca247256621feabcd380d7e76e72cfe62280c
Anant13T <ananttayal13@gmail.com>
2022-07-06 13:22:39 +0530

    3

--- GUI/ENCDEC.py
+++ GUI/ENCDEC.py
@@ -8,40 +8,12 @@ from Crypto.Cipher import AES
 from secretsharing import PlaintextToHexSecretSharer
 from secretsharing import SecretSharer
 
-
-#BS = 16
-#pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS).encode()
-#unpad = lambda s: s[:-ord(s[len(s)-1:])]
-
-#GENERATE A (random hexadecimal number)
-#temporaryKey = binascii.b2a_hex(os.urandom(16))
-
-'''
-Method:
-	shamirs_split
-Args:
-	file object 
-Function:
-	This method splits the text content of the file using shamir's secret
-	sharing algorithm. A list of hex codes are generated. These hex codes
-	are later broken into three parts again. This list of list is returns
-	as output
-'''
 def shamirs_split(file_object):
 	text = file_object.read()
 	list = PlaintextToHexSecretSharer.split_secret(text,2,2)
 	hexcode = SecretSharer.split_secret(list[0][2:],2,2);
 	return hexcode,list[1]
 
-'''
-Method:
-	shamir's join
-Args:
-	list
-Function:
-	Converts the excrypted hexcodes into decrypted ones. Use those decrypted 
-	hexcode to decrypt the text. Returns the text.
-'''
 def shamirs_join(list,str):
 	temp = []
 	msg_alpha =  SecretSharer.recover_secret(list[0:2])
@@ -50,11 +22,6 @@ def shamirs_join(list,str):
 	temp.append(str)
 	text = PlaintextToHexSecretSharer.recover_secret(temp[0:2])
 	return text
-
-'''
-Method:
-	AES encryption
-'''
 
 class AESCipher(object):
     BS = 16
@@ -66,17 +33,9 @@ class AESCipher(object):
 
     @staticmethod
     def iv():
-        """
-        The initialization vector to use for encryption or decryption.
-        It is ignored for MODE_ECB and MODE_CTR.
-        """
         return chr(0) * 16
 
     def encrypt(self, message):
-        """
-        It is assumed that you use Python 3.0+, so plaintext's type must be str type(== unicode).
-        """
-        #message = message.encode()
         raw = self.pad(message)
         cipher = AES.new(self.key.encode('utf-8'), AES.MODE_CBC, self.iv().encode('utf-8'))
         enc = cipher.encrypt(raw)
